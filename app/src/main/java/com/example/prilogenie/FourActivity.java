ef6c9178d60838795cc78ec369011af62c9b9fee
Roman <githubroman@gmail.com>
2023-05-07 13:31:08 +0400

    ispravil xml i sdelal perexod na 4 ctivnost

--- app/src/main/java/com/example/prilogenie/FourActivity.java
+++ app/src/main/java/com/example/prilogenie/FourActivity.java
@@ -6,9 +6,5 @@ import android.os.Bundle;
 
 public class FourActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_four);
-    }
+
 }
